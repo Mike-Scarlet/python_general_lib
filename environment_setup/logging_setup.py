5d6a064109f46180bd07af4f741db3cec615acae
Mike-Scarlet <mike_scarlet@126.com>
2023-02-19 11:02:49 +0800

    stah

--- environment_setup/logging_setup.py
+++ environment_setup/logging_setup.py
@@ -2,6 +2,13 @@
 import logging
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s %(message)s",
-    datefmt="[%X]",
+    format="[%(asctime)s] %(message)s",
+    # datefmt="[%X]",
 )
+
+def LoggingAddFileHandler(file_path):
+    handler = logging.FileHandler(file_path, "a", encoding='utf-8')
+    formatter = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+    handler.setFormatter(formatter)
+    root_logger = logging.getLogger()
+    root_logger.addHandler(handler)
