47f273b3ec948e256f4dca4ab82cf65393b24843
mikeisgreat <mikeisgreat@126.com>
2023-08-28 20:10:52 +0800

    add quick sqlite db2

--- python_general_lib/database/single_model_sqlite_database.py
+++ python_general_lib/database/single_model_sqlite_database.py
@@ -1,11 +1,14 @@
 
 from SQLiteWrapper import *
 from python_general_lib.interface.json_serializable import *
+import typing
 
 class SingleModelSQLiteDatabase:
-  def __init__(self, db_path, model_class, primary_keys) -> None:
+  model_class: typing.ClassVar[IJsonSerializable]
+  def __init__(self, db_path: str, model_class, primary_keys: typing.Union[str, typing.List[str]]) -> None:
     self.db_path = db_path
     self.model_class = model_class
+    self.table_name = model_class.__name__
     self.primary_keys = primary_keys
 
     self.conn = None
@@ -14,15 +17,14 @@ class SingleModelSQLiteDatabase:
   def Initiate(self):
     # initiate by model class
     # create a temp model object
-    table_name = self.model_class.__name__
-    db_initiate_dict = {table_name: {"field_definition": {}}}
+    db_initiate_dict = {self.table_name: {"field_definition": {}}}
     temp_obj = self.model_class()
     for name, python_type in self.model_class.__annotations__.items():
       field_str = SQLField.GetSQLTypeFromPythonType(python_type)
       if isinstance(getattr(temp_obj, name), python_type) and python_type != str:   # string is not enabled
         field_str += " DEFAULT {}".format(getattr(temp_obj, name))
-      db_initiate_dict[table_name]["field_definition"][name] = field_str
-    db_initiate_dict[table_name]["primary_keys"] = self.primary_keys
+      db_initiate_dict[self.table_name]["field_definition"][name] = field_str
+    db_initiate_dict[self.table_name]["primary_keys"] = self.primary_keys
     # print(json.dumps(db_initiate_dict, indent=2))
 
     db_table_structure = SQLDatabase.CreateFromDict(db_initiate_dict)
@@ -33,6 +35,19 @@ class SingleModelSQLiteDatabase:
     self.conn.TableValidation()
 
     self.op = SQLite3Operator(self.conn)
+
+  def InsertRecord(self, item: IJsonSerializable, or_condition: str=""):
+    insert_dict = item.ToJson()
+    self.op.InsertDictToTable(insert_dict, self.table_name, or_condition)
+
+  def QueryRecords(self, query_condition: str=None):
+    raw_records = self.op.SelectFieldFromTable("*", self.table_name, query_condition)
+    results = []
+    for record in raw_records:
+      item = self.model_class()
+      item.FromJson(record)
+      results.append(item)
+    return results
 
 if __name__ == "__main__":
   class TestClass:
