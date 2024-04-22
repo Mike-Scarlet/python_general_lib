d03c2bed3e368d4dfcf48eedcbdb78b130a4232b
mikeisgreat <mikeisgreat@126.com>
2024-04-22 16:08:20 +0800

    add rotate log

--- python_general_lib/environment_setup/logging_setup.py
+++ python_general_lib/environment_setup/logging_setup.py
@@ -1,5 +1,6 @@
 
 import logging
+import logging.handlers
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] %(message)s",
@@ -8,6 +9,13 @@ logging.basicConfig(
 
 def LoggingAddFileHandler(file_path):
     handler = logging.FileHandler(file_path, "a", encoding='utf-8')
+    formatter = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+    handler.setFormatter(formatter)
+    root_logger = logging.getLogger()
+    root_logger.addHandler(handler)
+
+def LoggingAddTimedRotatingFileHandler(file_path, when="d", interval=1, backup_count=0):
+    handler = logging.handlers.TimedRotatingFileHandler(file_path, when=when, interval=interval, backupCount=backup_count, encoding='utf-8')
     formatter = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
     handler.setFormatter(formatter)
     root_logger = logging.getLogger()
